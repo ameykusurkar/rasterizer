7bf0365e4994f675dea62fda0864fb7063d057a5
Amey Kusurkar <amey1000@gmail.com>
2020-12-20 21:26:22 +0000

    Draw and fill triangles

--- main.go
+++ main.go
@@ -37,34 +37,96 @@ func (c *Canvas) PutPixel(x, y float32) {
 	c.image.Set(cX, cY, color.RGBA{0xFF, 0xFF, 0xFF, 0xFF})
 }
 
-func (c *Canvas) DrawLine(p0, p1 Vertex) {
+// TODO: Is this the most efficient way?
+func Interpolate(p0, p1 Vertex) []Vertex {
 	dy, dx := p1.y-p0.y, p1.x-p0.x
 	if math.Abs(float64(dx)) > math.Abs(float64(dy)) {
-		if p0.x > p1.x {
-			p0, p1 = p1, p0
-		}
-		a := dy / dx
-		y := p0.y
-		for x := p0.x; x <= p1.x; x++ {
-			c.PutPixel(x, y)
-			y += a
-		}
+		return InterpolateHorizontal(p0, p1)
 	} else {
-		if p0.y > p1.y {
-			p0, p1 = p1, p0
-		}
-		a := dx / dy
-		x := p0.x
-		for y := p0.y; y <= p1.y; y++ {
-			c.PutPixel(x, y)
-			x += a
+		return InterpolateVertical(p0, p1)
+	}
+}
+
+func InterpolateHorizontal(p0, p1 Vertex) []Vertex {
+	verts := make([]Vertex, 0)
+	dy, dx := p1.y-p0.y, p1.x-p0.x
+	if p0.x > p1.x {
+		p0, p1 = p1, p0
+	}
+	a := dy / dx
+	y := p0.y
+	for x := p0.x; x <= p1.x; x++ {
+		verts = append(verts, Vertex{x, y})
+		y += a
+	}
+	return verts
+}
+
+func InterpolateVertical(p0, p1 Vertex) []Vertex {
+	verts := make([]Vertex, 0)
+	dy, dx := p1.y-p0.y, p1.x-p0.x
+	if p0.y > p1.y {
+		p0, p1 = p1, p0
+	}
+	a := dx / dy
+	x := p0.x
+	for y := p0.y; y <= p1.y; y++ {
+		verts = append(verts, Vertex{x, y})
+		x += a
+	}
+	return verts
+}
+
+func (c *Canvas) DrawLine(p0, p1 Vertex) {
+	for _, vert := range Interpolate(p0, p1) {
+		c.PutPixel(vert.x, vert.y)
+	}
+}
+
+func (c *Canvas) DrawTriangle(p0, p1, p2 Vertex) {
+	c.DrawLine(p0, p1)
+	c.DrawLine(p1, p2)
+	c.DrawLine(p2, p0)
+}
+
+func (c *Canvas) FillTriangle(p0, p1, p2 Vertex) {
+	if p1.y < p0.y {
+		p0, p1 = p1, p0
+	}
+	if p2.y < p0.y {
+		p0, p2 = p2, p0
+	}
+	if p2.y < p1.y {
+		p1, p2 = p2, p1
+	}
+
+	x01 := InterpolateVertical(p0, p1)
+	x01 = x01[:len(x01)-1] // Last value overlaps with x12
+	x12 := InterpolateVertical(p1, p2)
+	x02 := InterpolateVertical(p0, p2)
+	x012 := append(x01, x12...)
+
+	var xLeft, xRight []Vertex
+	if x01[len(x01)-1].x < x02[len(x01)-1].x {
+		xLeft, xRight = x012, x02
+	} else {
+		xLeft, xRight = x02, x012
+	}
+
+  // TODO: Understand why we cannot use DrawLine here
+	for i := 0; i < len(x02); i++ {
+		left, right := xLeft[i].x, xRight[i].x
+		for x := left; x <= right; x++ {
+			c.PutPixel(x, xLeft[i].y)
 		}
 	}
 }
 
 func (c *Canvas) Update() error {
 	c.Clear()
-	c.DrawLine(randomVertex(), randomVertex())
+	p0, p1, p2 := randomVertex(), randomVertex(), randomVertex()
+	c.DrawTriangle(p0, p1, p2)
+	c.FillTriangle(p0, p1, p2)
 	return nil
 }
 
@@ -89,7 +151,7 @@ func (c *Canvas) Layout(outsideWidth, outsideHeight int) (int, int) {
 
 func main() {
 	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
-	ebiten.SetWindowTitle("Noise (Ebiten Demo)")
+	ebiten.SetWindowTitle("Rasterizer")
 	ebiten.SetMaxTPS(10) // Slow enough to visualise the random lines
 	c := &Canvas{
 		image: image.NewRGBA(image.Rect(0, 0, screenWidth, screenHeight)),
